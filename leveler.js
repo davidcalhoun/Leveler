c2dcfbbc06f864981c449152a1b94d74a49ca7c1
David Calhoun <david.b.calhoun@gmail.com>
2010-05-14 23:48:15 -0700

    added Closure compiler notes

--- leveler.js
+++ leveler.js
@@ -3,6 +3,7 @@ if(!Date.now) {
     Date.now = function() {return (new Date().getTime())};
 }
 
+// WARNING: Closure Compiler doesn't preserve "trim" here
 // String.trim() (from Crockford)
 if(typeof String.prototype.trim !== 'function') {
     String.prototype.trim = function() {
